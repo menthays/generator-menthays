21db901941b30b3e47e00d2e6d85d04d015c95ff
Hao Yang <tillohao@gmail.com>
2018-03-04 16:37:14 +0800

    add user interactions (appname/username/description)

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,10 +6,7 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
-
     this.argument('appname', { type: String, required: true });
-
-    this.log(this.options.appname);
   }
 
   prompting() {
@@ -18,23 +15,30 @@ module.exports = class extends Generator {
       'Welcome to the sweet ' + chalk.red('generator-menthays') + ' generator!'
     ));
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option? I change it?',
-      default: true
-    }];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+    return this.prompt([{
+      type    : 'input',
+      name    : 'name',
+      message : 'Your project name',
+      default : this.options.appname // Default to current folder name
+    }, {
+      type    : 'input',
+      name    : 'username',
+      message : 'What\'s your GitHub username',
+      store   : true
+    }, {
+      type    : 'input',
+      name    : 'description',
+      message : 'Some description pls',
+    }]).then((props) => {
+      this.props = props
     });
   }
 
   writing() {
-    this.fs.copy(
+    this.fs.copyTpl(
       this.templatePath('./'),
-      this.destinationPath('./')
+      this.destinationPath(`./${this.options['appname']}/`),
+      {...this.props}
     );
   }
 
