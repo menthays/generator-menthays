0cc60654075825785608637f9a0b5d5395984a34
Hao Yang <tillohao@gmail.com>
2018-03-09 16:51:23 +0800

    fixed some bugs

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,43 +6,72 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
-    this.argument('appname', { type: String, required: true });
+    this.argument('appname', {
+      type: String,
+      required: true
+    });
+    this.option('vanilla')
+    this.option('react')
+
+    this.mode = this.options.react ? 'react' : 'vanilla'
   }
 
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the sweet ' + chalk.red('generator-menthays') + ' generator!'
-    ));
+    if (this.mode === 'react') {
+      this.log(yosay(
+        'Welcome to the sweet ' + chalk.red('generator-menthays') + ' generator!\n' +
+        'You have choose react scaffold. This one is based on create-react-app and integrated with sass and ant-design'
+      ));
+    } else {
+      this.log(yosay(
+        'Welcome to the sweet ' + chalk.red('generator-menthays') + ' generator!\n' 
+      ));
 
-    return this.prompt([{
-      type    : 'input',
-      name    : 'name',
-      message : 'Your project name',
-      default : this.options.appname // Default to current folder name
-    }, {
-      type    : 'input',
-      name    : 'username',
-      message : 'What\'s your GitHub username',
-      store   : true
-    }, {
-      type    : 'input',
-      name    : 'description',
-      message : 'Some description pls',
-    }]).then((props) => {
-      this.props = props
-    });
+      return this.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: this.options.appname // Default to current folder name
+      }, {
+        type: 'input',
+        name: 'username',
+        message: 'What\'s your GitHub username',
+        store: true
+      }, {
+        type: 'input',
+        name: 'description',
+        message: 'Some description pls',
+      }]).then((props) => {
+        this.props = props
+      });
+    }
+
   }
 
   writing() {
-    this.fs.copyTpl(
-      this.templatePath('./'),
-      this.destinationPath(`./${this.options['appname']}/`),
-      {...this.props}
-    );
+    if (this.mode === 'react') {
+      this.fs.copyTpl(
+        this.templatePath('./react-scaffold'),
+        this.destinationPath(`./${this.options['appname']}/`), { ...this.props
+        }
+      );
+    } else {
+      this.fs.copyTpl(
+        this.templatePath('./vanilla-scaffold'),
+        this.destinationPath(`./${this.options['appname']}/`), { ...this.props
+        }
+      );
+    }
+
   }
 
   install() {
-    this.installDependencies();
+    process.chdir(process.cwd()+'/'+this.options.appname);
+    this.installDependencies({
+      npm: true,
+      bower: false,
+      yarn: false
+    });
   }
 };
---
+++ generators/app/templates/vanilla-scaffold/README.md
@@ -0,0 +1,2 @@
+## Description
+Frontend scaffold for multi-page web project, integrated with webpack/sass/jest/babel/postcss.
