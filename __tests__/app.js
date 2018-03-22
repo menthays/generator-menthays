5e5accbf891471646ed43b6dbeea1b88d757dcea
Hao Yang <tillohao@gmail.com>
2018-03-22 14:33:26 +0800

    add auto test and eslint fix(npm run fix)

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,15 +3,31 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-describe('generator-menthays:app', () => {
+describe('generator-menthays:vanilla', () => {
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withArguments(['generator-temp'])
+      .withPrompts({
+        name: 'test',
+        username: 'Menthays <tillohao@gmail.com>',
+        description: 'test with jest'
+      });
+  });
+
+  it('created and CD into a folder named like the generator', () => {
+    assert.equal(path.basename(process.cwd()), 'generator-temp');
   });
 
   it('creates files', () => {
     assert.file([
-      'dummyfile.txt'
+      'webpack.config.js',
+      'webpack.utils.js',
+      'postcss.config.js',
+      'package.json',
+      './.babelrc',
+      './.editorconfig',
+      './.gitignore'
     ]);
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -10,64 +10,74 @@ module.exports = class extends Generator {
       type: String,
       required: true
     });
-    this.option('vanilla')
-    this.option('react')
+    this.option('vanilla');
+    this.option('react');
 
-    this.mode = this.options.react ? 'react' : 'vanilla'
+    this.mode = this.options.react ? 'react' : 'vanilla';
   }
 
   prompting() {
     // Have Yeoman greet the user.
     if (this.mode === 'react') {
-      this.log(yosay(
-        'Welcome to the sweet ' + chalk.red('generator-menthays') + ' generator!\n' +
-        'You have choose react scaffold. This one is based on create-react-app and integrated with sass and ant-design'
-      ));
+      this.log(
+        yosay(
+          'Welcome to the sweet ' +
+            chalk.red('generator-menthays') +
+            ' generator!\n' +
+            'You have choose react scaffold. This one is based on create-react-app and integrated with sass and ant-design'
+        )
+      );
     } else {
-      this.log(yosay(
-        'Welcome to the sweet ' + chalk.red('generator-menthays') + ' generator!\n' 
-      ));
+      this.log(
+        yosay('Welcome to the sweet ' + chalk.red('generator-menthays') + ' generator!\n')
+      );
 
-      return this.prompt([{
-        type: 'input',
-        name: 'name',
-        message: 'Your project name',
-        default: this.options.appname // Default to current folder name
-      }, {
-        type: 'input',
-        name: 'username',
-        message: 'What\'s your GitHub username',
-        store: true
-      }, {
-        type: 'input',
-        name: 'description',
-        message: 'Some description pls',
-      }]).then((props) => {
-        this.props = props
+      return this.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Your project name',
+          default: this.options.appname // Default to current folder name
+        },
+        {
+          type: 'input',
+          name: 'username',
+          message: "What's your GitHub username",
+          store: true
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Some description pls'
+        }
+      ]).then(props => {
+        this.props = props;
       });
     }
-
   }
 
   writing() {
     if (this.mode === 'react') {
       this.fs.copyTpl(
         this.templatePath('./react-scaffold'),
-        this.destinationPath(`./${this.options['appname']}/`), { ...this.props
+        this.destinationPath(`./${this.options.appname}/`),
+        {
+          ...this.props
         }
       );
     } else {
       this.fs.copyTpl(
         this.templatePath('./vanilla-scaffold'),
-        this.destinationPath(`./${this.options['appname']}/`), { ...this.props
+        this.destinationPath(`./${this.options.appname}/`),
+        {
+          ...this.props
         }
       );
     }
-
   }
 
   install() {
-    process.chdir(process.cwd()+'/'+this.options.appname);
+    process.chdir(process.cwd() + '/' + this.options.appname);
     this.installDependencies({
       npm: true,
       bower: false,
