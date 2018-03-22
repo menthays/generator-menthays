5b93e6635500a836e42c4c14968be2d43560f40c
Hao Yang <tillohao@gmail.com>
2018-03-22 14:57:54 +0800

    add auto test for react generator

--- __tests__/app.js
+++ __tests__/app.js
@@ -25,9 +25,37 @@ describe('generator-menthays:vanilla', () => {
       'webpack.utils.js',
       'postcss.config.js',
       'package.json',
-      './.babelrc',
-      './.editorconfig',
-      './.gitignore'
+      '.babelrc',
+      '.editorconfig',
+      '.gitignore'
+    ]);
+  });
+});
+
+describe('generator-menthays:react', () => {
+  beforeAll(() => {
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withArguments(['generator-temp-react'])
+      .withOptions({ react: true })
+      .withPrompts({
+        name: 'test',
+        username: 'Menthays <tillohao@gmail.com>',
+        description: 'test with jest'
+      });
+  });
+
+  it('created and CD into a folder named like the generator', () => {
+    assert.equal(path.basename(process.cwd()), 'generator-temp-react');
+  });
+
+  it('creates files', () => {
+    assert.file([
+      'public/index.html',
+      'src/index.js',
+      'config-overrides.js',
+      'package.json',
+      '.gitignore'
     ]);
   });
 });
